a37f9fbd5153d6e87f6d64e81a831f508d92605f
GabMartino <gabriele-martino@libero.it>
2018-11-27 00:07:22 +0100

    Made the structure of the simulation and implemented the protocol.

---
+++ Epidemic_Broadcast/Node.cc
@@ -0,0 +1,142 @@
+
+//        Performance Evaluation Project
+//
+//        Epidemic Broadcast
+//
+//        Authors:
+//          Gabriele Martino - Maurizio Pulizzi - Antonio Di Tecco
+//
+//
+
+#include "Node.h"
+
+Define_Module(Node);
+
+void Node::initialize()
+{
+    // Take the slotTime and retransmissionProbability parameters from the network parameters
+    slotTime = getParentModule()->par("slotTime").doubleValue();
+    retransmissionProbability = getParentModule()->par("P").doubleValue();
+
+    // if this is the starter node
+    if(this->getIndex() == getParentModule()->par("indexOfStartingNode").intValue()){
+        //being the starting node is also infected
+        infected = true;
+        transmitted = true;
+
+        //change icon of the simulation to show the infection (is used with hasGUi to distiguish graphic or console simulation)
+        if(hasGUI()){
+            cDisplayString& displayString = getDisplayString();
+            displayString.setTagArg("i", 0, "block/broadcast");
+        }
+
+        // make the first message and broadcast
+        cMessage* msg = new cMessage("Broadcast");
+        broadcastMessage(msg);
+    }
+}
+
+/*
+ * This method handle the messages arrived
+ */
+void Node::handleMessage(cMessage *msg)
+{
+    // this node is not infected: collisions happend
+    if(!infected){
+
+        // a selfMessage points that a slot time of transmission is finished
+        if(msg->isSelfMessage()){
+
+           // if in a slot time has been received only a message, everything is ok
+           if(messageCounter == 1){
+               infected = true;
+               bubble("INFECTED");
+
+               //change icon of the simulation to show the infection
+               if(hasGUI()){
+                   cDisplayString& displayString = getDisplayString();
+                   displayString.setTagArg("i", 0, "block/circle");
+               }
+
+               // try to transmitt
+               tryToSend(messageToRetransmit);
+           }else{// otherwise collision: other messages arrived in a slotTime
+               bubble("COLLISION");
+
+               //reset counter
+               messageCounter = 0;
+
+               //delete the stored message
+               delete messageToRetransmit;
+           }
+       }else{// New message is arrived
+
+           // count messages arrived
+           messageCounter++;
+           if(messageCounter == 1){ // first attempt
+               // storing of the message
+               messageToRetransmit = msg->dup();
+
+               // The transmission time is a slot so a selfmessage is sent when the transmission is finished
+               scheduleAt(simTime() + slotTime, new cMessage("Reception finished"));
+           }
+       }
+    }else if(!transmitted && infected){// this node is infected and tries to broadcast infection
+        tryToSend(messageToRetransmit);
+    }
+
+    // delete anyway the message: if a new one, 'cause has been stored, if is collision one or if is a selfMessage one
+    delete msg;
+
+}
+
+/*
+ * This method takes a message and tries to transmit with a certain probability
+ * otherwise postpones to the next slot another try
+ */
+void Node::tryToSend(cMessage* msg){
+
+    // extracts a 0 or a 1 with a Bernuallian probability setted
+    int check = bernoulli(retransmissionProbability);
+
+    if(check){// broadcast message
+        EV<< "Transmission in progress"<<endl;
+
+        // set a variable of transmission
+        transmitted = true;
+        bubble("TRANSMITTED");
+
+        // broadcast the message
+        broadcastMessage(msg);
+
+        // delete the stored message
+        delete messageToRetransmit;
+    }else{
+        EV<< "Try to transmitt next time"<<endl;
+
+        // Postpone another try to the next slot
+        scheduleAt(simTime() + slotTime, new cMessage("Retry to send"));
+    }
+
+}
+
+/*
+ * This method broadcasts a message to all output gates connected.
+ *
+ */
+void Node::broadcastMessage(cMessage* msg){
+    /*
+     * Given that we used inout gates and we have doplicated gate.
+     * We need only out gate so we divided by 2 the number
+     * SEE IF THERE'S A METHOD THAT RETURNS ONLY THE OUTPUT GATE, OR USE AN ITERATOR
+     *
+     */
+    int numGateOut = this->gateCount()/2;
+
+    for(int i= 0; i < numGateOut; i++){
+        cMessage* copy = msg->dup();
+        send(copy, "gate$o",i);
+    }
+
+}
+
---
+++ Epidemic_Broadcast/out/gcc-release/Node.d
@@ -0,0 +1,6 @@
+out/gcc-release//Node.o: Node.cc Node.h \
+ /home/gabriele/Desktop/omnetpp-5.4.1/include/omnetpp.h
+
+Node.h:
+
+/home/gabriele/Desktop/omnetpp-5.4.1/include/omnetpp.h:
